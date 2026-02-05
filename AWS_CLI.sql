964cb5429f49392e674f315a1e04cbf1e72656ee
João Pedro de Sousa Silveira <72753914+joaopedrosilveira@users.noreply.github.com>
2026-02-05 15:08:04 -0300

    Update AWS_CLI.sql

--- AWS_CLI.sql
+++ AWS_CLI.sql
@@ -1,4 +1,10 @@
 --Comando AWS CLi para subir uma pasta no bucket
+
+--Configurando o ambiente
+aws configure set default.s3.multipart_chunksize 128MB
+
+aws configure set default.s3.max_concurrent_requests 20
+ 
 -- Caso a pasta ja exista esse comando so adiciona a diferença de arquivos
 Comando: aws s3 sync H:\BACKUP_SRV2042 s3://bkp-prd-cnseg/BACKUP_SRV2042 --debug
 
